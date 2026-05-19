c40bedae630288c37f2bd35cdae0d237b0dff369
joicy roslin <joicyroslin@gmail.com>
2026-05-19 22:33:55 +0530

    core (main) : resolve conflicts and sync latest changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,10 @@
-import 'reflect-metadata';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-const port = process.env.PORT ?? 3001;
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -18,10 +16,9 @@ const port = process.env.PORT ?? 3001;
     }),
   );
 
-  await app.listen(port);
-
   const port = process.env.PORT || 3000;
 
   await app.listen(port);
 }
+
 bootstrap();
